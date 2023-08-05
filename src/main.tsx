97083dc9ffe92d583266f567770d177271b4c23c
Moyo <31387340+MoyosoreCodes@users.noreply.github.com>
2023-08-05 20:05:26 +0100

    minor changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { makeServer } from "./server/index.ts";
+import { makeServer } from "./server";
 
 if (process.env.NODE_ENV === "development") {
   makeServer({ environment: "development" });
